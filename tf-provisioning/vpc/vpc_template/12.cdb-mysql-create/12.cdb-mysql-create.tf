1353b666603b9049aeb4906e37cd0872715edf23
CGKang <user@CGKangui-Macbook-Pro.local>
2021-06-15 16:17:08 +0900

    210615_cdb pub -> cdb priv

--- tf-provisioning/vpc/vpc_template/12.cdb-mysql-create/12.cdb-mysql-create.tf
+++ tf-provisioning/vpc/vpc_template/12.cdb-mysql-create/12.cdb-mysql-create.tf
@@ -2,9 +2,9 @@ data "ncloud_vpc" "vpc" {
   name = "tf-${var.account_name}-vpc${var.vpc_num}"
 }
 
-data "ncloud_subnet" "vpc_pub_subnet" {
+data "ncloud_subnet" "vpc_priv_subnet" {
   vpc_no = data.ncloud_vpc.vpc.id
-  subnet = var.pub_subnet_cidr
+  subnet = var.priv_subnet_cidr
 }
 
 resource "null_resource" "cdb-mysql-create" {
@@ -12,7 +12,7 @@ resource "null_resource" "cdb-mysql-create" {
         working_dir = var.path_module
         
         command = <<EOF
-${var.path_module}/cdb_mysql.sh GET "/vmysql/v2/createCloudMysqlInstance?vpcNo=${data.ncloud_vpc.vpc.id}&cloudMysqlImageProductCode=${var.cdbmysql_imagecode}&cloudMysqlProductCode=${var.cdbmysql_prdcode}&isHa=${var.cdbmysql_isHa}&isMultiZone=${var.cdbmysql_isMultiZone}&cloudMysqlServiceName=${var.cdbmysql_name}&cloudMysqlServerNamePrefix=${var.cdbmysql_prefix}&cloudMysqlUserName=${var.cdbmysql_username}&cloudMysqlUserPassword=${var.cdbmysql_userpwd}&hostIp=${var.cdbmysql_hostIP}&cloudMysqlPort=${var.cdbmysql_port}&cloudMysqlDatabaseName=${var.cdbmysql_dbname}&subnetNo=${data.ncloud_subnet.vpc_pub_subnet.id}&responseFormatType=json" >> ${var.path_module}/output.apistate
+${var.path_module}/cdb_mysql.sh GET "/vmysql/v2/createCloudMysqlInstance?vpcNo=${data.ncloud_vpc.vpc.id}&cloudMysqlImageProductCode=${var.cdbmysql_imagecode}&cloudMysqlProductCode=${var.cdbmysql_prdcode}&isHa=${var.cdbmysql_isHa}&isMultiZone=${var.cdbmysql_isMultiZone}&cloudMysqlServiceName=${var.cdbmysql_name}&cloudMysqlServerNamePrefix=${var.cdbmysql_prefix}&cloudMysqlUserName=${var.cdbmysql_username}&cloudMysqlUserPassword=${var.cdbmysql_userpwd}&hostIp=${var.cdbmysql_hostIP}&cloudMysqlPort=${var.cdbmysql_port}&cloudMysqlDatabaseName=${var.cdbmysql_dbname}&subnetNo=${data.ncloud_subnet.vpc_priv_subnet.id}&responseFormatType=json" >> ${var.path_module}/output.apistate
 
         EOF
     }
--- tf-provisioning/vpc/vpc_template/13.cdb-mysql-delete/13.cdb-mysql-delete.tf
+++ tf-provisioning/vpc/vpc_template/13.cdb-mysql-delete/13.cdb-mysql-delete.tf
@@ -2,9 +2,9 @@ data "ncloud_vpc" "vpc" {
   name = "tf-${var.account_name}-vpc${var.vpc_num}"
 }
 
-data "ncloud_subnet" "vpc_pub_subnet" {
+data "ncloud_subnet" "vpc_priv_subnet" {
   vpc_no = data.ncloud_vpc.vpc.id
-  subnet = var.pub_subnet_cidr
+  subnet = var.priv_subnet_cidr
 }
 
 
@@ -13,7 +13,7 @@ resource "null_resource" "cdb-mysql-delete" {
         working_dir = var.path_module
         
         command = <<EOF
-CDB_NUM=$(${var.path_module}/cdb_mysql.sh GET "/vmysql/v2/getCloudMysqlInstanceList?vpcNo=${data.ncloud_vpc.vpc.id}&subnetNo=${data.ncloud_subnet.vpc_pub_subnet.id}&cloudMysqlServiceName=${var.cdbmysql_name}&responseFormatType=json" |grep cloudMysqlInstanceNo |cut -d "\"" -f4)
+CDB_NUM=$(${var.path_module}/cdb_mysql.sh GET "/vmysql/v2/getCloudMysqlInstanceList?vpcNo=${data.ncloud_vpc.vpc.id}&subnetNo=${data.ncloud_subnet.vpc_priv_subnet.id}&cloudMysqlServiceName=${var.cdbmysql_name}&responseFormatType=json" |grep cloudMysqlInstanceNo |cut -d "\"" -f4)
 
 ${var.path_module}/cdb_mysql.sh POST "/vmysql/v2/deleteCloudMysqlInstance?cloudMysqlInstanceNo=$CDB_NUM&responseFormatType=json"
         EOF
