7ee0c292f773d19a552130474ea92184a73f505b
chikuk.park <chikuk.park@navercorp.com>
2021-06-16 18:58:34 +0900

    nas 인스턴스 번호 파일로 남기게 수정

--- tf-provisioning/vpc/vpc_template/make_tf.sh
+++ tf-provisioning/vpc/vpc_template/make_tf.sh
@@ -38,6 +38,7 @@ nas_number=`./create-nas.sh $nas_name $nas_size $nas_type`
 if [ "$nas_number" = "Failed" ]; then
     echo "Make Nas Failed"
 else
+    echo $nas_number > nas_number
     server1_No=`cat ../09.server-exechost/terraform.tfstate  | grep \"instance_no\" | awk -F\" '{print $4}'`
     ./access-control.sh set $nas_number $server1_No
 fi
